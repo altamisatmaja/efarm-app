ed85b3322e429854e8f92947f7fa72b51576eaea
altamisatmaja <altamisfattah16@gmail.com>
2024-03-04 15:10:25 +0700

    fix: page

--- lib/blocs/bloc.dart
+++ lib/blocs/bloc.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
 part 'homepage_bloc/homepage_bloc.dart';
 part 'homepage_bloc/homepage_event.dart';
 part 'homepage_bloc/homepage_state.dart';
--- lib/views/pages/page.dart
+++ lib/views/pages/page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:ppl_app/utils/util.dart';
 
 part 'sign_in_page.dart';
 part 'sign_up_page.dart';
